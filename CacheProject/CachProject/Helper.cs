c8a53c79286ab0db84b8240c0d94e2ed52cd461b
Ehab Shaker <ehab-shaker319@outlook.com>
2024-01-01 20:31:09 +0300

    Update Helper class with new data

--- CacheProject/CachProject/Helper.cs
+++ CacheProject/CachProject/Helper.cs
@@ -1,19 +1,34 @@
-﻿namespace CacheProject
+﻿using CacheProject.Classes;
+
+namespace CacheProject
 {
     public static class Helper
     {
-        static string[] names = new[] { "C#", "java", "delphi", "javascript", "python", "sql", "oracle", "dotnet", "linq", "react" };
-        static int[] ids = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-        static public async Task<List<string>> SearchNames(string toSearch)
+        static List<Data> data = new()
+        {
+            new Data{  Category = 1,Type ="A",Value = "Value1 | Cat: 1 | Type : A"},
+            new Data{  Category = 1,Type ="B",Value = "Value2 | Cat: 1 | Type : B"},
+            new Data{  Category = 1,Type ="C",Value = "Value3 | Cat: 1 | Type : C"},
+            new Data{  Category = 2,Type ="A",Value = "Value4 | Cat: 2 | Type : A"},
+            new Data{  Category = 2,Type ="B",Value = "Value5 | Cat: 2 | Type : B"},
+            new Data{  Category = 3,Type ="B",Value = "Value6 | Cat: 3 | Type : B"},
+            new Data{  Category = 3,Type ="C",Value = "Value7 | Cat: 3 | Type : C"},
+            new Data{  Category = 3,Type ="D",Value = "Value8 | Cat: 3 | Type : D"},
+            new Data{  Category = 3,Type ="E",Value = "Value9 | Cat: 3 | Type : E"},
+        };
+        static public async Task<List<DataByType>> Search(string toSearch)
         {
             await Task.Delay(2000);//Time delay simulation
-            return names.Where(x => x.Contains(toSearch)).ToList();
+            return data.Where(x => x.Type == toSearch)
+                .Select(t => new DataByType { Type = t.Type, Value = t.Value })
+                .ToList();
         }
-        static public async Task<List<int>> SearchIds(int toSearch)
+        static public async Task<List<DataByCategory>> Search(int toSearch)
         {
             await Task.Delay(2000);//Time delay simulation
-            return ids.Where(x => x > toSearch).ToList();
+            return data.Where(x => x.Category == toSearch)
+                .Select(t => new DataByCategory { Category = t.Category, Value = t.Value })
+                .ToList();
         }
     }
 }
