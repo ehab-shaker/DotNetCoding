1cc967e5c50e1b1eaae90b867dd2767c195f7a85
Ehab Shaker <ehab-shaker319@outlook.com>
2024-01-01 20:35:25 +0300

    fix files name

--- CacheProject/CachProject/CasheService/CacheSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CachProject.CashService
-{
-    public class CacheSettings
-    {
-        public bool Enabled { get; set; } = true;
-        public double AbsoluteExpiration { get; set; } = 600;
-        public double SlidingExpiration { get; set; } = 300;
-    }
-}
